897e67c331167c53f8034f1b24c3c90933604cd8
Miguel Angel Perez Jr <PerezMiguelAngel@proton.me>
2025-10-14 15:43:01 -0700

    Set up boot scene w/game manager

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -10,14 +11,18 @@ public class GameController : MonoBehaviour
         if (gameController == null)
         {
             gameController = this;
-            
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
     // Update is called once per frame
